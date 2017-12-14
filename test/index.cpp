4522841889ce496d5dabcbba40113fcd28b93e45
once-ler <htao@eztier.com>
2017-12-13 19:11:02 -0500

    Add simple example

---
+++ test/index.cpp
@@ -0,0 +1,40 @@
+#include "tds-client.h"
+#include <map>
+
+using namespace std;
+
+int spec_0(map<const string&, const string&> sqlconf, const string& script) {
+  auto db = tds::TDSClient();
+  int rc;
+
+  rc = db.connect(sqlconf["host"], sqlconf["user"], sqlconf["pass"]);
+	if (rc)
+		return rc;
+
+  rc = db.useDatabase(sqlconf["database"]);
+	if (rc)
+		return rc;
+
+  db.sql(script);
+
+	rc = db.execute();
+
+	if (rc) {
+		return rc;
+	}
+
+	return 0;  
+}
+
+int main() {
+
+  int rc = 0;
+
+  {
+    rc = spec_0({{"host", "127.0.0.1"}, {"user", "guest"}, {"pass", "1234"}}, "select current_timestamp;");
+    if (rc != 0) {
+      throw ("Spec 0 failed.");
+    }
+  }
+
+}
